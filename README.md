f6a1b2d21c061680dd4a2f45c2f6422298f21838
Shaheen Rajith <shaheen.rajith@outlook.in>
2025-03-11 14:23:08 +0000

    Fixed some fundamental errors in the code, updated README, added the users SQL database and added ability to add entries to the users db by filling the new user registration form. Login now checks with the users db and only logs you in if you have registered already

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
  Hey all, this is the Github Repo for our Security and Privacy Group Project, so far there isnt much here. the env folder has the yml and txt file needed to created a new "SECPRI" conda env with some needed modules and the trial.py file is just a basic sample hello world webpage to check if the flask module was working and is generating web pages which it is. I need to now make the actual web page for the e-commerce site.
 
+As of now, the Login Page, New User Registration Page and the Users SQL db is done, whats left is the Main Store Page, Checkout Page and Transactions SQL db.
+
 All the code was typed on my Dell XPS Laptop running Ubuntu 24.04 LTS. so you run something and random errors appear due to OS or computer differences. I have no idea how to fix them.
--- Website.py
+++ Website.py
@@ -1,34 +1,84 @@
-from flask import Flask, render_template, request, redirect, session
-import pysqlite3
+from flask import Flask, render_template, request, redirect, session, url_for
+import mysql.connector
 import json
-# ALL CAPS COMMENTS ARE FUTURE WORK
+
 app = Flask(__name__)
 app.secret_key = 'totallysecretkey'
 
-def init_db():
-    # NEED TO SETUP SQL DATABASES HERE
-    i=0
 
-    
+# Setting up connection with SQL Server
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password
+        )
+        print("MySQL Database connection successful")
+    except:
+        print(f"Error")
+    return connection
+server_connection = create_server_connection("localhost", "root","12345") #Hardcoded, varies per person
+cursor = server_connection.cursor()
+
+
+# Setting up Databases incase they dont already exist
+def init_db():
+    cursor.execute("""
+        USE secpri
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            email VARCHAR(50) UNIQUE NOT NULL,
+            password VARCHAR(50) NOT NULL
+        )
+    """)
+    server_connection.commit()
+init_db()
+
+
+# Main Login Page
 @app.route('/', methods =['GET','POST'])
 def home():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-    # CHECK WITH TABLE AND PROCEED IF VALID
+        log_email = request.form['email']
+        log_password = request.form['password']
+        try:
+            cursor.execute(f"SELECT * FROM users WHERE email = '{log_email}' AND password = '{log_password}'")
+            user = cursor.fetchone()
+            if user:
+                session["user_Id"] = user[0]
+                return redirect(url_for('shop'))
+            else:
+                print("INVALID Email / Password Combination")
+                return redirect(url_for('home'))
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return redirect(url_for('home'))
     # Displaying Login Page upon entering the site
     return render_template("login.html")
 
+
+# New User Registration Page
 @app.route('/register', methods =['GET','POST'])
 def register():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-    # ADD USERNAME AND PASSWORD TO TABLE
+        reg_email = request.form['email']
+        reg_password = request.form['password']
+        try:
+            cursor.execute(f"INSERT INTO users (email, password) VALUES ('{reg_email}','{reg_password}')")
+            server_connection.commit()
+            return redirect(url_for('home'))
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
     # Displaying Registration Page for new Users
     return render_template("register.html")
 
-@app.route('/login', methods =['GET','POST'])
+
+# Shop Page after Logging in Successfully
+@app.route('/shop', methods =['GET','POST'])
 def shop():
     return """<p> WORK IN PROGRESS, COME BACK LATER :) </p>"""
 
