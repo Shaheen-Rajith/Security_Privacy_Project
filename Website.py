a45ef11d739d4557b8cd56e8c68bf770b6222077
Shaheen Rajith <shaheen.rajith@outlook.in>
2025-03-11 06:55:11 +0000

    Added a new user registration page so that one can make new acounts

--- Website.py
+++ Website.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 import pysqlite3
 import json
-
+# ALL CAPS COMMENTS ARE FUTURE WORK
 app = Flask(__name__)
 app.secret_key = 'totallysecretkey'
 
@@ -10,12 +10,28 @@ def init_db():
     i=0
 
     
-@app.route('/')
+@app.route('/', methods =['GET','POST'])
 def home():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    # CHECK WITH TABLE AND PROCEED IF VALID
     # Displaying Login Page upon entering the site
     return render_template("login.html")
 
+@app.route('/register', methods =['GET','POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    # ADD USERNAME AND PASSWORD TO TABLE
+    # Displaying Registration Page for new Users
+    return render_template("register.html")
+
+@app.route('/login', methods =['GET','POST'])
+def shop():
+    return """<p> WORK IN PROGRESS, COME BACK LATER :) </p>"""
+
+
 if __name__ == "__main__":
     app.run()
-
-"------------------------------------ WORK IN PROGRESS ------------------------------------"
